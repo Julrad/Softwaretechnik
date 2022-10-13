1f798b6654bf5bcbcb7fc1487d4f92721e413f70
Julius <Julius@Julius_Laptop.fritz.box>
2022-10-13 11:12:46 +0200

    update

--- Hello_World/src/de/uni/koeln/se/Main.java
+++ Hello_World/src/de/uni/koeln/se/Main.java
@@ -3,5 +3,7 @@ package de.uni.koeln.se;
 public class Main {
 	public static void main(String[] args) {
 		System.out.print("Hello0o0o0o World!");
+		
+		System.out.print("Go0o0dbye World!");
 	}
 }
